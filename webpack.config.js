92843fbf052a4688e31f2882e534e67ff5946388
Maksym Sharpan <maksim.sharpan@gmail.com>
2021-03-29 12:33:11 +0300

    add webpack-config-file

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html', inject: 'body' }),
+  ],
+
+  devServer: {
+    port: 8888,
+    open: true,
+    stats: 'errors-only',
+  },
+};
